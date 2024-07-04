cc8add9d0107e3cb9e122306ebb36bc51bfeafed
Connor Wales <connorwales@gmail.com>
2024-07-04 14:00:20 -0700

    complete initial schema of models/Issue

---
+++ models/Issue.ts
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+
+// basic interface
+export interface IIssue {
+  name: string,
+  description: string,
+  type?: "component" | "page",
+  route_location?: string,
+  design_figma_link?: string,
+  eng_team_gh_issue_link?: string,
+  eng_team_files: string[],
+  user_stories: mongoose.Types.ObjectId[],
+  // merge checklist
+  design_complete: boolean,
+  eng_implementation_complete: boolean,
+  eng_implementation_meets_design: {
+    meets_design: boolean,
+    approving_designer: mongoose.Types.ObjectId,
+    approval_date: Date
+  }
+
+}
+
+// declare instance methods, define later
+export interface IIssueMethods {
+
+}
+
+export interface IssueModel extends mongoose.Model<IIssue, {}, IIssueMethods> {
+
+}
+
+// main schema
+const IssueSchema = new mongoose.Schema<IIssue, IssueModel, IIssueMethods>({ 
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ["component", "page"],
+    required: false
+  },
+  route_location: {
+    type: String,
+    required: false
+  },
+  design_figma_link: {
+    type: String,
+    required: false
+  },
+  eng_team_gh_issue_link: {
+    type: String,
+    required: false
+  },
+  eng_team_files: [{
+    type: String,
+  }],
+  user_stories: [{
+    type: mongoose.Types.ObjectId,
+    ref: "UserStory"
+  }],
+  // merge checklist
+  design_complete: {
+    type: Boolean,
+    default: false
+  },
+  eng_implementation_complete: {
+    type: Boolean,
+    default: false
+  },
+  eng_implementation_meets_design: {
+    type: {
+      meets_design: {
+        type: Boolean,
+        default: false
+      },
+      approving_designer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      approval_date: {
+        type: Date,
+        required: false
+      }
+    }
+  }
+})
+
+// back references
+IssueSchema.virtual('user_stories', {
+  ref: "UserStory",
+  localField: "_id",
+  foreignField: "issue"
+})
+
+// define instance methods
+
+
+
+// define static methods
+
+
+
+// export model
+export default mongoose.models.Issue as IssueModel || mongoose.model<IIssue, IssueModel>("Issue", IssueSchema)
