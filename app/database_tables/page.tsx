110b0e2ccd4f0a8e199eeddab2e273c69aed325b
Connor Wales <connorwales@gmail.com>
2024-07-08 12:39:26 -0700

    heading and import cleanup in database_tables/page

--- app/database_tables/page.tsx
+++ app/database_tables/page.tsx
@@ -1,30 +1,21 @@
-import Paper from "@mui/material/Paper";
-import Accordion from "@mui/material/Accordion"
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Typography from '@mui/joy/Typography';
 import { getPopulatedTables, PopulatedTableType } from "@/models/Controls";
-import { Table, TableHead, TableRow, TableCell, TableBody, Box, useTheme, Grid } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import DBTableAccordion from "@/components/DBTableAccordion";
 
 
 export default async function Page() {
 
   const tables: PopulatedTableType[] = await getPopulatedTables();
-  // console.log(JSON.stringify(tables, null, 2));
-
-
 
   return (
     <>
-      <h1>Tables</h1>
+      <Typography level="h1">Database Tables</Typography>
       <Grid container>
         {tables.map(table => {
           return (
             <Grid item xs={12} sm={12} md={6}>
             <DBTableAccordion 
-              // table={table} 
               table_id={table._id.toString()}
               table_name={table.name}
               table_properties={table.table_properties.map(tp => {
