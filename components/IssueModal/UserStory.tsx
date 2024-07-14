e943d4b93faea02760c38cc8a47299ba29a22264
Connor Wales <connorwales@gmail.com>
2024-07-13 17:51:03 -0700

    add edit UserStory button and modal access to components/IssueModal/UserStory, add state handling and styling to components/IssueModal/UserStoryDialog, more to do

--- components/IssueModal/UserStory.tsx
+++ components/IssueModal/UserStory.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { TableCell, TableHead, TableRow, Table, Typography, TableBody, Checkbox, TextField, Chip, Button } from "@mui/material"
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import BlockIcon from '@mui/icons-material/Block';
@@ -6,27 +7,30 @@ import { useEditIssueContext } from "@/context/EditIssueContext"
 import QueryTooltip from "./QueryTooltip";
 import { v4 as uuidv4 } from 'uuid';
 import EditIcon from '@mui/icons-material/Edit';
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import UserStoryDialog from "./UserStoryDialog";
+import Issue from "@/models/Issue";
 
 
 
 type UserStoryProps = {
-  story: IUserStory & {_id: string}
+  story: IUserStory & { _id: string }
   showUserStoryDialog: boolean,
   setShowUserStoryDialog: Dispatch<SetStateAction<boolean>>
 }
 
-export default function UserStory({story}: UserStoryProps) {
+export default function UserStory({ story }: UserStoryProps) {
 
   const { editMode } = useEditIssueContext();
+  const [showUserStoryDialog, setShowUserStoryDialog] = useState(false);
 
   const joinedDescription = story.description.map(des => {
     if (des.textType === "string") {
       return des.text
     } else return (
-      <QueryTooltip 
+      <QueryTooltip
         textType={des.textType}
-        text={des.text}   
+        text={des.text}
         key={uuidv4()}
       />
     )
@@ -37,36 +41,53 @@ export default function UserStory({story}: UserStoryProps) {
   // </span>
 
 
+  const {
+    description, engineering_done, design_done, database_references, links, components
+  } = story;
+
+  const issue = story.issue.toString();
+
 
   return (
     <TableRow key={story._id}>
-        {
-          editMode && 
+      {
+        editMode &&
 
-          <TableCell sx={{borderBottom: "none"}}>
-            <Button>
-              <EditIcon 
-                color="primary"
-              />
-            </Button>
-          </TableCell>
-          // <TextField
-          //   defaultValue={story.description.map(des => des.text).join("")}
-          //   variant="standard"
-          //   multiline
-          //   fullWidth
-          //   maxRows={2}
-          // /> :
-            
+        <TableCell sx={{ borderBottom: "none" }}>
+          <Button
+            onClick={()=>setShowUserStoryDialog(true)}
+          >
+            <EditIcon
+              color="primary"
+            />
+          </Button>
+          <UserStoryDialog
+            issueId={story.issue.toString()}
+            prop_id={story._id}
+            prop_story={{
+              issue, description, engineering_done, design_done, database_references, links, components
+            }}
+            open={showUserStoryDialog}
+            onClose={() => setShowUserStoryDialog(false)}
+          />
+        </TableCell>
+        // <TextField
+        //   defaultValue={story.description.map(des => des.text).join("")}
+        //   variant="standard"
+        //   multiline
+        //   fullWidth
+        //   maxRows={2}
+        // /> :
 
-          
-        }
 
-      <TableCell sx={{borderBottom: "none"}}>
+
+      }
+
+      <TableCell sx={{ borderBottom: "none" }}>
         <span>{joinedDescription}</span>
       </TableCell>
-      <TableCell 
-         sx={{
+      <TableCell
+        sx={{
           width: "10rem",
           borderBottom: "none",
           textAlign: "center"
@@ -75,12 +96,12 @@ export default function UserStory({story}: UserStoryProps) {
         {editMode ?
           <Checkbox defaultChecked={story.design_done} /> :
 
-          story.design_done ? 
-            <CheckCircleIcon /> : 
+          story.design_done ?
+            <CheckCircleIcon /> :
             <BlockIcon />
         }
       </TableCell>
-      <TableCell 
+      <TableCell
         sx={{
           width: "10rem",
           borderBottom: "none",
@@ -90,8 +111,8 @@ export default function UserStory({story}: UserStoryProps) {
         {editMode ?
           <Checkbox defaultChecked={story.engineering_done} /> :
 
-          story.engineering_done ? 
-            <CheckCircleIcon /> : 
+          story.engineering_done ?
+            <CheckCircleIcon /> :
             <BlockIcon />
         }
       </TableCell>
--- components/IssueModal/UserStoryDialog.tsx
+++ components/IssueModal/UserStoryDialog.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { IUserStory } from "@/models/UserStory";
-import { Dialog, DialogTitle, DialogContent, Modal } from "@mui/material";
-import { useState } from "react";
+import { Dialog, DialogTitle, DialogContent, Box, FormControl, Select, InputLabel, MenuItem, TextField, Stack, Table, TableRow, TableCell } from "@mui/material";
+import { useState, useEffect, ReactNode } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import QueryTooltip from "./QueryTooltip";
 
 
-type EditableUserStory = Omit<IUserStory, "issue"> & { issue: string } 
+type EditableUserStory = Omit<IUserStory, "issue"> & { issue: string }
 
 
 type UserStoryDialogProps = {
@@ -17,20 +19,10 @@ type UserStoryDialogProps = {
 
 
 export default function UserStoryDialog(
-  {issueId, prop_id, prop_story, open, onClose}: UserStoryDialogProps
+  { issueId, prop_id, prop_story, open, onClose }: UserStoryDialogProps
 ) {
 
-  const blah: EditableUserStory = {
-    issue: issueId,
-    description: [],
-    engineering_done: false,
-    design_done: false,
-    database_references: [],
-    links: [],
-    components: []
-  }
-
-  const [currentStory, setStory] = useState<EditableUserStory>(prop_story || {
+  const [story, setStory] = useState<EditableUserStory>(prop_story || {
     issue: issueId,
     description: [],
     engineering_done: false,
@@ -40,12 +32,163 @@ export default function UserStoryDialog(
     components: []
   })
 
+  const [joinedDescription, setJoinedDescription] = useState<ReactNode>(getUpdatedDescription())
+
+  // const joinedDescription = story.description.map(des => {
+  //   if (des.textType === "string") {
+  //     return des.text
+  //   } else return (
+  //     <QueryTooltip
+  //       textType={des.textType}
+  //       text={des.text}
+  //       key={uuidv4()}
+  //     />
+  //   )
+  // })
+
+  useEffect(() => {
+    const joinedDescription = (
+      <>
+        {story.description.map(des => {
+          if (des.textType === "string") {
+            return des.text
+          } else return (
+            <QueryTooltip
+              textType={des.textType}
+              text={des.text}
+              key={uuidv4()}
+            />
+          )
+        })}
+      </>
+    )
+  }, [story])
+
+
+  function updateStoryDescriptionTextType(index: number, value: "string" | "tableRef" | "tablePropertyRef" | "pageRef" | "componentRef") {
+    const description = story.description;
+    description[index].textType = value;
+
+    setStory({
+      ...story,
+      description
+    })
+  }
+
+  function updateStoryDescriptionText(index: number, value: string) {
+    const description = story.description;
+    description[index].text = value;
+
+    setStory({
+      ...story,
+      description
+    })
+  }
+
+
+  // function updateStory(field: "textType" | "text", index: number, value: string) {
+
+  //   const description = story.description;
+
+  //   description[i][field] = value;
+
+  //   setStory({
+  //     ...story,
+  //     description
+  //   })
+  // }
+
+
+  // function changeDescriptionType(descriptionType: string, index: number) {
+  //   story.description[index]
+  // }
+
+  function updateJoinedDescription() {
+    setJoinedDescription(getUpdatedDescription())
+  }
+
+  function getUpdatedDescription() {
+    return (
+      story.description.map(des => {
+        if (des.textType === "string") {
+          return des.text
+        } else return (
+          <QueryTooltip
+            textType={des.textType}
+            text={des.text}
+            key={uuidv4()}
+          />
+        )
+      })
+    )
+  }
+
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth
+      maxWidth="md"
+    >
       <>
         <DialogTitle>{prop_id ? "Edit " : "Create "}User Story</DialogTitle>
         <DialogContent>
-          {/* <Box /> */}
+          <Box sx={{
+            border: "1px solid black",
+            margin: 4,
+            padding: 1
+          }}>
+            {joinedDescription}
+          </Box>
+          {/* <Stack direction="column" gap={1}> */}
+          <Table>
+            {story.description.map((description, i) => {
+              return (
+                // <Stack direction="row" key={i} spacing={1}>
+                <TableRow>
+                  {/* <FormControl fullWidth > */}
+                  <TableCell>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      variant="standard"
+                      value={description.textType}
+                      // sx={maxWidth}
+                      label="Type"
+                      onChange={(e) => {
+                        updateStoryDescriptionTextType(i, e.target.value as "string" | "tableRef" | "tablePropertyRef" | "pageRef" | "componentRef")
+                      }}
+                    >
+                      <MenuItem value={"string"}>Text</MenuItem>
+                      <MenuItem value={"tableRef"}>Table Reference</MenuItem>
+                      <MenuItem value={"tablePropertyRef"}>Table Property Reference</MenuItem>
+                      <MenuItem value={"pageRef"}>Page Reference</MenuItem>
+                      <MenuItem value={"componentRef"}>Component Reference</MenuItem>
+                    </Select>
+                  </TableCell>
+
+                  {/* <InputLabel>Text</InputLabel> */}
+                  <TableCell>
+                    <TextField
+                      value={description.text}
+                      variant="standard"
+                      multiline
+                      label="Text"
+                      fullWidth
+                      maxRows={2}
+                      onChange={(e) => {
+                        updateStoryDescriptionText(i, e.target.value);
+
+                      }}
+                    />
+                    {/* </FormControl> */}
+                    {/* </Stack> */}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </Table>
+          {/* </Stack> */}
         </DialogContent>
       </>
     </Dialog>
