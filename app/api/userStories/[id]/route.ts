5965a4c98e51e118e06ba50e32aeab0553ed9b14
Connor Wales <connorwales@gmail.com>
2024-07-21 13:43:29 -0700

    clean up comments and logs in api/userStories/route.ts, modify api/userStories/[id]/route.ts to use same population strategy, client now successfully retrieving updated issue

--- app/api/userStories/[id]/route.ts
+++ app/api/userStories/[id]/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from "next/server";
 import UserStory from "@/models/UserStory";
-
-
+import { IUserStory } from "@/models/UserStory";
+import { IUser } from "@/models/User";
+import mongoose from "mongoose";
+import Issue from "@/models/Issue";
 
 
 export async function POST(req: NextRequest, {params} : {params: {id: string}}) {
@@ -9,12 +11,34 @@ export async function POST(req: NextRequest, {params} : {params: {id: string}}) 
   try {
 
     const {story} = await req.json();
-    console.log(JSON.stringify({"incoming user story" : story}, null, 2));
 
+    const userStory = await UserStory.findByIdAndUpdate(params.id, story)
+    .populate({
+      path: 'issue',
+      select: '_id name description issueType assigned_designers assigned_engineers route_location design_figma_link eng_team_gh_issue_link eng_team_files design_complete eng_implementation_meets_design',
+      populate: [
+        {
+          path: 'user_stories',
+          select: '_id issue description database_references links components'
+        },
+        {
+          path: 'assigned_designers',
+          select: '_id email imageUrl registered active'
+        },
+        {
+          path: 'assigned_engineers',
+          select: '_id email imageUrl registered active'
+        },
+        {
+          path: 'eng_implementation_meets_design.approving_designer',
+          select: '_id email imageUrl registered active'
+        }
+      ]
+    })
 
-    const userStory = await UserStory.findByIdAndUpdate(params.id, story);
+    if (!userStory) throw new Error("userStory not found");
 
-    return Response.json(userStory);
+    return Response.json(userStory.issue);
 
   } catch(err) {
     console.log(err);
--- app/api/userStories/route.ts
+++ app/api/userStories/route.ts
@@ -12,17 +12,8 @@ export async function POST(req: NextRequest, {params} : {params: {id: string}}) 
   try {
 
     const {story} = await req.json();
-    console.log(JSON.stringify({"incoming user story" : story}, null, 2));
 
     const userStory = await UserStory.create(story);
-
-    // const issue = await userStory.populate<IIssue & {_id: mongoose.Types.ObjectId}>({
-    //   path: 'issue',
-    //   select: '_id name description issueType assigned_designers assigned_engineers route_location design_figma_link eng_team_gh_issue_link eng_team_files design_complete eng_implementation_meets_design'
-    // })
-
-    // console.log(JSON.stringify({issue}, null, 2));
-
 
     await userStory.populate({
       path: 'issue',
@@ -47,62 +38,7 @@ export async function POST(req: NextRequest, {params} : {params: {id: string}}) 
       ]
     })
 
-    
-    // await issue
-    //   .populate<{user_stories: Array<IUserStory & {_id: mongoose.Types.ObjectId}>}>({
-    //     path: 'user_stories',
-    //     select: '_id issue description database_references links components'
-    //   })
-    // await issue.populate<{assigned_designers: Array<IUser & {_id: mongoose.Types.ObjectId}>}>({
-    //   path: 'assigned_designers',
-    //   select: '_id email imageUrl registered active'
-    // })
-    // await issue.populate<{assigned_engineers: Array<IUser & {_id: mongoose.Types.ObjectId}>}>({
-    //   path: 'assigned_engineers',
-    //   select: '_id email imageUrl registered active'
-    // }),
-    // await issue.populate<{eng_implementation_meets_design: {
-    //   meets_design: boolean,
-    //   approving_designer: IUser & {
-    //     _id: mongoose.Types.ObjectId
-    //   },
-    //   approval_date: Date
-    // }}>({
-    //   path: 'eng_implementation_meets_design.approving_designer',
-    //   select: '_id email imageUrl registered active'
-    // })
-
-
-
-    // await Promise.all([
-    //   userStory.populate<{user_stories: Array<IUserStory & {_id: mongoose.Types.ObjectId}>}>({
-    //     path: 'user_stories',
-    //     select: '_id issue description database_references links components'
-    //   }),
-    //   userStory.populate<{assigned_designers: Array<IUser & {_id: mongoose.Types.ObjectId}>}>({
-    //     path: 'assigned_designers',
-    //     select: '_id email imageUrl registered active'
-    //   }),
-    //   userStory.populate<{assigned_engineers: Array<IUser & {_id: mongoose.Types.ObjectId}>}>({
-    //     path: 'assigned_engineers',
-    //     select: '_id email imageUrl registered active'
-    //   }),
-    //   userStory.populate<{eng_implementation_meets_design: {
-    //     meets_design: boolean,
-    //     approving_designer: IUser & {
-    //       _id: mongoose.Types.ObjectId
-    //     },
-    //     approval_date: Date
-    //   }}>({
-    //     path: 'eng_implementation_meets_design.approving_designer',
-    //     select: '_id email imageUrl registered active'
-    //   })
-    // ]);
-
-    console.log(JSON.stringify({userStory}, null, 2));
-
-    return Response.error();
-    // return Response.json(userStory.issue);
+    return Response.json(userStory.issue);
 
   } catch(err) {
     console.log(err);
