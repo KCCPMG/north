9b6e5f829d212987306f8996efe376b75730ecbe
Connor Wales <connorwales@gmail.com>
2024-07-31 22:30:48 -0700

    add text, link, styling to LoginHome component on main page

--- components/LoginHome.tsx
+++ components/LoginHome.tsx
@@ -1,7 +1,11 @@
 "use client";
-import { signIn, getProviders, ClientSafeProvider, LiteralUnion } from "next-auth/react";
+import { signIn, getProviders, ClientSafeProvider } from "next-auth/react";
 import { useState, useEffect } from "react";
-import { Box, Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 
 
@@ -19,17 +23,42 @@ export default function LoginHome() {
   }, [])
 
   return (
-    <>
-      {providers && Object.values(providers).map(provider => {
-        return (
-          <Box key={provider.name}>
-            <Button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </Button>
-          </Box>
-        )
-      })}
-    
-    </>
+    <Stack
+      sx={{
+        maxWidth: 650,
+        marginX: 'auto',
+        marginTop: 10
+      }}
+      spacing={2}
+    >
+      <Box sx={{ width: '100%' }} alignItems="left">
+        <Typography variant="h1">North</Typography>
+        <Typography variant="h2">
+          A Project Management Tool for Compass
+        </Typography>
+      </Box>
+      <Stack alignItems="center" spacing={3}>
+        <Box>
+          <Typography variant="body1">
+            Welcome to North! If you would like to be able to edit and create issues, please log in. Doing so will automatically register you as a user, and also allow you to be assigned issues. If you would like to only view without editing, please click the bottom link to be directed to the Issues page as a viewer only.
+          </Typography>
+        </Box>
+        <Box>
+          {providers && Object.values(providers).map(provider => {
+            return (
+              <Box key={provider.name}>
+                <Button variant="contained" onClick={() => signIn(provider.id)}>
+                  Sign in with {provider.name}
+                </Button>
+              </Box>
+            )
+          })}
+        </Box>
+        <Box>
+
+          <Link href="/issues">Continue without Logging In</Link>
+        </Box>
+      </Stack>
+    </Stack>
   )
 }
