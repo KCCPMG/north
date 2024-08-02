cc3b83109b026fcf44a07d1bc842dbd8b268200b
Connor Wales <connorwales@gmail.com>
2024-08-02 10:20:48 -0700

    refactor app/issues/page to retrieve issues and then pass them into client component components/NewIssueDialog, where all logic now is, to allow for state and issue updates

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,85 +1,16 @@
-import { Typography, Box, Stack, Divider } from "@mui/material";
-import { IssueContextProvider } from "@/context/IssueContext";
 import { ParsedPopulatedIssueType, getPopulatedIssues } from "@/models/Controls";
-import IssueCard from "@/components/IssueCard";
 import mongooseConnect from "@/lib/mongooseConnect";
+import IssuesPageContent from "@/components/IssuesPageContent";
 
 export default async function Page() {
 
   await mongooseConnect();
   const issues = await getPopulatedIssues();
   const parsedIssues: Array<ParsedPopulatedIssueType> = JSON.parse(JSON.stringify(issues));
-
-  const inDesignIssues: Array<ParsedPopulatedIssueType> = [];
-  const inProgressIssues: Array<ParsedPopulatedIssueType> = []; 
-  const mergingIssues: Array<ParsedPopulatedIssueType> = []; 
-  const completeIssues: Array<ParsedPopulatedIssueType> = [];
-
-
-  // sort out issues
-  while (parsedIssues.length > 0) {
-    const issue = parsedIssues.shift() as ParsedPopulatedIssueType;
-    if (issue?.eng_implementation_meets_design.meets_design === true) {
-      completeIssues.push(issue);
-    } else if (issue?.user_stories.every(us => us.design_done && us.engineering_done)) {
-      mergingIssues.push(issue);
-    } else if (issue?.assigned_designers?.length > 0 && issue?.assigned_engineers.length > 0) {
-      inProgressIssues.push(issue);
-    } else inDesignIssues.push(issue);
-  }
-
-  const columnTuples: Array<[string, Array<ParsedPopulatedIssueType>]> = [
-    ["In Design", inDesignIssues],
-    ["In Progress", inProgressIssues],
-    ["Merging", mergingIssues],
-    ["Complete", completeIssues]
-  ]
-
-  const columns = columnTuples.map(tup => {
-    return (
-      <Stack 
-        direction="column" 
-        width={1/4} 
-        key={tup[0].toLowerCase().replaceAll(" ", "-")}
-      >
-        <Stack direction="row" sx={{
-          width: 1,
-          justifyContent: "center",
-          paddingY: 2
-        }}>
-          <Typography sx={{marginX: "auto"}} variant="h5">{tup[0]}</Typography>
-        </Stack>
-        <Divider variant="fullWidth" />
-        {tup[1].map(issue => (
-          <IssueContextProvider 
-            initialIssue={issue} 
-            key={issue._id.toString()}
-          >
-            <IssueCard initialIssue={issue} key={issue._id} />
-          </IssueContextProvider>
-        ))}
-      </Stack>
-    )
-  })
-
+  console.log(parsedIssues, new Date());
 
   return (
-    <>
-      <Typography sx={{paddingBottom: 3}} variant="h1">
-        Issues
-      </Typography>
-      <Box width={"100%"}>
-        <Stack 
-          direction="row" 
-          spacing={3} 
-          flexShrink={1}
-          flexGrow={1}
-          justifyContent="space-around"
-          divider={<Divider orientation="vertical" flexItem/>}
-        >
-          {columns}
-        </Stack>
-      </Box>
-    </>
+    <IssuesPageContent parsedIssues={parsedIssues} />
   )
+  
 }
---
+++ components/IssuesPageContent.tsx
@@ -0,0 +1,129 @@
+"use client";
+import { Typography, Box, Stack, Divider, Button } from "@mui/material";
+import { IssueContextProvider } from "@/context/IssueContext";
+import { ParsedPopulatedIssueType, getPopulatedIssues } from "@/models/Controls";
+import IssueCard from "@/components/IssueCard";
+import { useState, useEffect } from "react";
+
+
+type IssuesPageContentProps = {
+  parsedIssues: Array<ParsedPopulatedIssueType>
+}
+
+export default function IssuesPageContent(
+  { parsedIssues }: IssuesPageContentProps
+) {
+
+
+
+  // const inDesignIssues: Array<ParsedPopulatedIssueType> = [];
+  // const inProgressIssues: Array<ParsedPopulatedIssueType> = []; 
+  // const mergingIssues: Array<ParsedPopulatedIssueType> = []; 
+  // const completeIssues: Array<ParsedPopulatedIssueType> = [];
+
+  const [issues, setIssues] = useState<Array<ParsedPopulatedIssueType>>(parsedIssues) 
+
+  const [inDesignIssues, setInDesignIssues] = useState<Array<ParsedPopulatedIssueType>>([]);
+  const [inProgressIssues, setInProgressIssues] = useState<Array<ParsedPopulatedIssueType>>([]);
+  const [mergingIssues, setMergingIssues] = useState<Array<ParsedPopulatedIssueType>>([]);
+  const [completeIssues, setCompleteIssues] = useState<Array<ParsedPopulatedIssueType>>([]);
+
+
+  useEffect(() => {
+    // sort out issues
+    const issuesCopy = [...issues];
+
+    const completeIssuesCopy = [];
+    const mergingIssuesCopy = [];
+    const inProgressIssuesCopy = [];
+    const inDesignIssuesCopy = [];
+    
+    while (issuesCopy.length > 0) {
+      const issue = issuesCopy.shift() as ParsedPopulatedIssueType;
+      if (issue?.eng_implementation_meets_design.meets_design === true) {
+        completeIssuesCopy.push(issue);
+      } else if (issue?.user_stories.every(us => us.design_done && us.engineering_done)) {
+        mergingIssuesCopy.push(issue);
+      } else if (issue?.assigned_designers?.length > 0 && issue?.assigned_engineers.length > 0) {
+        inProgressIssuesCopy.push(issue);
+      } else inDesignIssuesCopy.push(issue);
+    }
+   
+    setCompleteIssues(completeIssuesCopy);
+    setMergingIssues(mergingIssuesCopy);
+    setInProgressIssues(inProgressIssuesCopy);
+    setInDesignIssues(inDesignIssuesCopy);
+
+  }, [issues])
+
+  // sort out issues
+  // while (parsedIssues.length > 0) {
+  //   const issue = parsedIssues.shift() as ParsedPopulatedIssueType;
+  //   if (issue?.eng_implementation_meets_design.meets_design === true) {
+  //     completeIssues.push(issue);
+  //   } else if (issue?.user_stories.every(us => us.design_done && us.engineering_done)) {
+  //     mergingIssues.push(issue);
+  //   } else if (issue?.assigned_designers?.length > 0 && issue?.assigned_engineers.length > 0) {
+  //     inProgressIssues.push(issue);
+  //   } else inDesignIssues.push(issue);
+  // }
+
+  const columnTuples: Array<[string, Array<ParsedPopulatedIssueType>]> = [
+    ["In Design", inDesignIssues],
+    ["In Progress", inProgressIssues],
+    ["Merging", mergingIssues],
+    ["Complete", completeIssues]
+  ]
+
+  return (
+    <>
+      <Stack justifyContent="space-between" direction="row">
+        <Typography sx={{paddingBottom: 3}} variant="h1">
+          Issues
+        </Typography>
+        <Button variant="outlined">
+          New Issue
+        </Button>
+      </Stack>
+      <Box width={"100%"}>
+        <Stack 
+          direction="row" 
+          spacing={3} 
+          flexShrink={1}
+          flexGrow={1}
+          justifyContent="space-around"
+          divider={<Divider orientation="vertical" flexItem/>}
+        >
+          {
+            columnTuples.map(tup => {
+              return (
+                <Stack 
+                  direction="column" 
+                  width={1/4} 
+                  key={tup[0].toLowerCase().replaceAll(" ", "-")}
+                >
+                  <Stack direction="row" sx={{
+                    width: 1,
+                    justifyContent: "center",
+                    paddingY: 2
+                  }}>
+                    <Typography sx={{marginX: "auto"}} variant="h5">{tup[0]}</Typography>
+                  </Stack>
+                  <Divider variant="fullWidth" />
+                  {tup[1].map(issue => (
+                    <IssueContextProvider 
+                      initialIssue={issue} 
+                      key={issue._id.toString()}
+                    >
+                      <IssueCard initialIssue={issue} key={issue._id} />
+                    </IssueContextProvider>
+                  ))}
+                </Stack>
+              )
+            })
+          }
+        </Stack>
+      </Box>
+    </>
+  )
+}
