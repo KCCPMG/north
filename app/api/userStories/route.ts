9f32024b3d66cfe9dcf96abbb27cac35912440c3
Connor Wales <connorwales@gmail.com>
2024-08-08 11:44:15 -0700

    refactor api/userStories POST to move logic to models/Controls, call in route

--- app/api/userStories/route.ts
+++ app/api/userStories/route.ts
@@ -7,6 +7,7 @@ import UserStory from "@/models/UserStory";
 import mongooseConnect from "@/lib/mongooseConnect";
 import checkSession from "@/lib/checkSession";
 import { revalidatePath } from "next/cache";
+import { createUserStoryAndPopulateIssue } from "@/models/Controls";
 
 
 
@@ -21,34 +22,38 @@ export async function POST(req: NextRequest, {params} : {params: {id: string}}) 
 
     const {story} = await req.json();
 
-    const userStory = await UserStory.create(story);
+    // const userStory = await UserStory.create(story);
 
-    await userStory.populate({
-      path: 'issue',
-      select: '_id name description issueType assigned_designers assigned_engineers route_location design_figma_link eng_team_gh_issue_link eng_team_files design_complete eng_implementation_meets_design',
-      populate: [
-        {
-          path: 'user_stories',
-          select: '_id issue description database_references links components engineering_done design_done'
-        },
-        {
-          path: 'assigned_designers',
-          select: '_id email imageUrl registered active'
-        },
-        {
-          path: 'assigned_engineers',
-          select: '_id email imageUrl registered active'
-        },
-        {
-          path: 'eng_implementation_meets_design.approving_designer',
-          select: '_id email imageUrl registered active'
-        }
-      ]
-    })
+    // await userStory.populate({
+    //   path: 'issue',
+    //   select: '_id name description issueType assigned_designers assigned_engineers route_location design_figma_link eng_team_gh_issue_link eng_team_files design_complete eng_implementation_meets_design',
+    //   populate: [
+    //     {
+    //       path: 'user_stories',
+    //       select: '_id issue description database_references links components engineering_done design_done'
+    //     },
+    //     {
+    //       path: 'assigned_designers',
+    //       select: '_id email imageUrl registered active'
+    //     },
+    //     {
+    //       path: 'assigned_engineers',
+    //       select: '_id email imageUrl registered active'
+    //     },
+    //     {
+    //       path: 'eng_implementation_meets_design.approving_designer',
+    //       select: '_id email imageUrl registered active'
+    //     }
+    //   ]
+    // })
+
+    const issue = await createUserStoryAndPopulateIssue(story);
+
+    if (!issue) throw new Error("Issue not found");
 
     revalidatePath('/issues', 'page');
 
-    return Response.json(userStory.issue);
+    return Response.json(issue);
 
   } catch(err) {
     console.log(err);
--- models/Controls.ts
+++ models/Controls.ts
@@ -151,3 +151,45 @@ export async function updateUserStoryAndPopulateIssue(user_story_id: string, sto
   }
 
 }
+
+
+export async function createUserStoryAndPopulateIssue(story: IUserStory): Promise<ParsedPopulatedIssueType | null> {
+
+  try {
+
+    const userStory = await UserStory.create(story);
+
+    const populatedUserStory = await userStory
+    .populate<Omit<IUserStory, 'issue'> & {issue: ParsedPopulatedIssueType}>
+    ({
+      path: 'issue',
+      select: '_id name description issueType assigned_designers assigned_engineers route_location design_figma_link eng_team_gh_issue_link eng_team_files design_complete eng_implementation_meets_design',
+      populate: [
+        {
+          path: 'user_stories',
+          select: '_id issue description database_references links components engineering_done design_done'
+        },
+        {
+          path: 'assigned_designers',
+          select: '_id email imageUrl registered active'
+        },
+        {
+          path: 'assigned_engineers',
+          select: '_id email imageUrl registered active'
+        },
+        {
+          path: 'eng_implementation_meets_design.approving_designer',
+          select: '_id email imageUrl registered active'
+        }
+      ]
+    })
+
+    return populatedUserStory?.issue || null;
+
+  } catch(err) {
+    console.log(err);
+    throw err;
+  }
+
+
+}
